9b20e8809d0cd180ec163ed70bb9d26c54c4e083
Joshua Low <Joshua Low>
2021-01-10 16:27:04 +0800

    final

--- App - Copy (2).js
+++ App - Copy (2).js
@@ -8,51 +8,27 @@ import {
   View,
 } from "react-native";
 
-const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=14191";
+const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [arrival, setArrival] = useState("");
-  const [arrivaltime, setArrivaltime] = useState("");
-
-  function dateConvert(time){
-    const day = new Date(time)
-    let [hour, minute, second] = day.toLocaleTimeString("en-US").split(":")
-    const timeArranged = `${hour}:${minute}:${second}`
-    return timeArranged
-  } 
 
   function loadBusStopData() {
-    setLoading(true);
-
     fetch(BUSSTOP_URL)
       .then((response) => response.json())
       .then((responseData) => {
         //console.log(responseData)
         const myBus = responseData.services.filter(
-          (item) => item.no === "195"
+          (item) => item.no === "155"
         )[0];
-        console.log(myBus);
-        const duration_s = Math.floor(myBus.next.duration_ms / 1000)
-        const minutes = Math.floor(duration_s / 60)
-        const seconds = duration_s % 60
-        setArrivaltime(dateConvert(myBus.next.time))
-
-        if (seconds < 0) {
-          setArrival("Oops, the bus just left. Don't be late for the next one!");
-          setArrivaltime(dateConvert(myBus.subsequent.time));}
-          else {
-          setArrival("Bus coming in " + `${minutes} minutes and ${seconds} seconds`);
-          setArrivaltime(dateConvert(myBus.next.time))}
-        setLoading(false);
-
-      });
+          setArrival(myBus.next.time);
+          setLoading(false);
+        });
   }
 
   useEffect(() => {
-    const interval = setInterval(loadBusStopData, 2000);
-
-    return () => clearInterval(interval);
+    loadBusStopData();
   }, []);
 
   return (
@@ -61,7 +37,6 @@ export default function App() {
       <Text style={styles.arrivalTime}>
         {loading ? <ActivityIndicator size="large" color="#aa8" /> : arrival}
       </Text>
-      <Text style={styles.arrivalTime}>{arrivaltime}</Text>
       <TouchableOpacity onPress={null} style={styles.button}>
         <Text style={styles.buttonText}>Refresh</Text>
       </TouchableOpacity>
--- App working.js
+++ App working.js
@@ -13,14 +13,6 @@ const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=14191";
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [arrival, setArrival] = useState("");
-  const [arrivaltime, setArrivaltime] = useState("");
-
-  function dateConvert(time){
-    const day = new Date(time)
-    let [hour, minute, second] = day.toLocaleTimeString("en-US").split(":")
-    const timeArranged = `${hour}:${minute}:${second}`
-    return timeArranged
-  } 
 
   function loadBusStopData() {
     setLoading(true);
@@ -30,23 +22,11 @@ export default function App() {
       .then((responseData) => {
         //console.log(responseData)
         const myBus = responseData.services.filter(
-          (item) => item.no === "195"
+          (item) => item.no === "57"
         )[0];
-        console.log(myBus);
-        const duration_s = Math.floor(myBus.next.duration_ms / 1000)
-        const minutes = Math.floor(duration_s / 60)
-        const seconds = duration_s % 60
-        setArrivaltime(dateConvert(myBus.next.time))
-
-        if (seconds < 0) {
-          setArrival("Oops, the bus just left. Don't be late for the next one!");
-          setArrivaltime(dateConvert(myBus.subsequent.time));}
-          else {
-          setArrival("Bus coming in " + `${minutes} minutes and ${seconds} seconds`);
-          setArrivaltime(dateConvert(myBus.next.time))}
-        setLoading(false);
-
-      });
+          setArrival(myBus.next.time);
+          setLoading(false);
+        });
   }
 
   useEffect(() => {
@@ -61,7 +41,6 @@ export default function App() {
       <Text style={styles.arrivalTime}>
         {loading ? <ActivityIndicator size="large" color="#aa8" /> : arrival}
       </Text>
-      <Text style={styles.arrivalTime}>{arrivaltime}</Text>
       <TouchableOpacity onPress={null} style={styles.button}>
         <Text style={styles.buttonText}>Refresh</Text>
       </TouchableOpacity>
